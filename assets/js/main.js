43f71ea460108f2e8059ce071bc4cf9fb9e9ddf2
Paraprax <parkerjohnston28@gmail.com>
2020-07-29 20:35:37 -0400

    reformatting in ES6 arrowhead style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var secretLetter = '';
+// array of possible letters:
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// secret letter var, to be assigned by function for each round:
+let secretLetter = '';
 
 // computer picks a letter at beginning of round:
-function letterSetter() {
-    var letterIndex = Math.floor(Math.random() * 26);
+const letterSetter = () => {
+    let letterIndex = Math.floor(Math.random() * 26);
     console.log(letterIndex);
 
     secretLetter = alphabet[letterIndex];
