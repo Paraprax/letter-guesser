4e98a74b487f08666134032d503c7453a0bb7a4f
Paraprax <parkerjohnston28@gmail.com>
2020-08-01 01:27:58 -0400

    comments mostly

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,23 @@ const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 let secretLetter = '';
 let userGuess = '';
 let guessedLetters = [];
-let guessesLeft = 8;
+let guessesLeft = 0;
 let winCount = 0;
 let lossCount = 0;
 let roundCount = 0;
 
 //game initializing function:
 const newRound = () => {
+    //~ ~ ~ ~ ~ ~ ~ at the beginning of each round:
+    //available guesses are reset to full:
+    guessesLeft = 8;
+
+    //a new secret letter is chosen by the program:
     letterSetter();
+
+    //the on-screen stats are all set to their stored values:
+    statUpdates();
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  ~ ~ ~ ~  ~ ~ ~ ~ 
 
     //whenever the user hits a key:
     document.onkeyup = function(event) {
@@ -22,16 +31,20 @@ const newRound = () => {
 
             //save the key value as the user's guess:
             userGuess = event.key;
-            
+
             //evaluate the user's guess && update var values:
             if (userGuess === secretLetter) {
+                //if the user guesses right, update the score and start a new round:
                 console.log('You win!');
                 winCount++;
+                newRound();
             } else {
+                //if the user guesses wrong, add their guess to the guessed-letters array and reduce the number of guesses left by 1:
                 guessedLetters.push(userGuess);
                 guessesLeft--;
             };
 
+            // update all the on-screen stats after each guess:
             statUpdates();
         }
     }
@@ -56,7 +69,6 @@ const statUpdates = () => {
     document.getElementById("loss-number").innerHTML = lossCount;
 }
 
-//start new round && update html stat elements on page load:
+//start new round on page load:
 newRound();
-statUpdates();
 
