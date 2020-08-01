057f4f225312222a0499239ed111d75a39498ae4
Paraprax <parkerjohnston28@gmail.com>
2020-08-01 01:07:50 -0400

    on-screen values now update after each guess

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 // secret letter var, to be assigned by function for each round:
 let secretLetter = '';
 let userGuess = '';
+let guessesLeft = 8;
 let winCount = 0;
 let lossCount = 0;
 let roundCount = 0;
@@ -17,18 +18,23 @@ const newRound = () => {
 
         //if the key is an actual letter(to prevent playing cmd, refresh, etc keys):
         if (alphabet.indexOf(event.key) != -1) {
+
+            //save the key value as the user's guess:
             userGuess = event.key;
-    
+            
+            //evaluate the user's guess && update var values:
             if (userGuess === secretLetter) {
                 console.log('You win!');
                 winCount++;
             } else {
                 console.log('You lose!');
-                lossCount++;
+                guessesLeft--;
             };
+
+            statUpdates();
+
         }
     }
-
 };
 
 // computer picks a letter at beginning of round:
@@ -41,10 +47,12 @@ const letterSetter = () => {
     return secretLetter;
 }
 
+// set/update html elements to reflect stat values:
 const statUpdates = () => {
-   document.getElementById("round-number").innerHTML = roundCount;
-   document.getElementById("win-number").innerHTML = winCount;
-   document.getElementById("loss-number").innerHTML = lossCount;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("round-number").innerHTML = roundCount;
+    document.getElementById("win-number").innerHTML = winCount;
+    document.getElementById("loss-number").innerHTML = lossCount;
 }
 
 //start new round && update html stat elements on page load:
